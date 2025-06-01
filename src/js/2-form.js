1ebce7741e750c0ac6f5bc32c943eca3bf522ae3
Ira <ira144ddd@gmail.com>
2025-06-01 21:57:50 +0200

    add hw

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,45 @@
-const storageKey = "feedback-form-state";
-const form = document.querySelector(".feedback-form");
-const textarea = document.querySelector("textarea");
-textarea.addEventListener("input", handleInput);
-function handleInput(event) {
-    const message = event.target.value;
-    localStorage.setItem(storageKey, message);
-}
-function populateTextArea() {
-    const message = localStorage.getItem(storageKey);
-    if(message) {
-        textarea.value = message;
+let formData = {
+    email: "",
+    message: "",
+  };
+  
+  const form = document.querySelector(".feedback-form");
+  const input = form.querySelector(".input");
+  const textarea = form.querySelector(".textarea");
+  const storageKey = "feedback-form-state";
+  
+  populateForm();
+  
+  form.addEventListener("input", handleInput);
+  form.addEventListener("submit", handleSubmit);
+  
+  function handleInput(event) {
+    const { name, value } = event.target;
+    formData[name] = value;
+    localStorage.setItem(storageKey, JSON.stringify(formData));
+  }
+  
+  function populateForm() {
+    const savedData = localStorage.getItem(storageKey);
+    if (savedData) {
+      formData = JSON.parse(savedData);
+      input.value = formData.email || "";
+      textarea.value = formData.message || "";
     }
-}
-
-
-form.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
+  }
+  
+  function handleSubmit(event) {
     event.preventDefault();
-    event.currentTarget.reset();
+    if (!formData.email.trim() || !formData.message.trim()) {
+      alert("Fill please all fields");
+      return;
+    }
+    console.log("Form submitted:", formData);
+    form.reset();
     localStorage.removeItem(storageKey);
-}
+    formData = {
+      email: "",
+      message: "",
+    };
+  }
+  
